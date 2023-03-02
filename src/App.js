086e5cce62c1256ddcb7df63acc67286c42d1590
Roshan4518Git <rdpadal@gmail.com>
2023-03-02 13:00:32 +0530

    changes made by me

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
-
+import demo from "./demo";
 function App() {
+  const nextPage = () => {
+    return (window.location.href = "/demo");
+  };
   return (
     <div className="App">
-      <h1>Hi my name is Roshan Padal</h1>
+      {/* <h1>Hi my name is Roshan Padal</h1> */}
+      <button onClick={nextPage}>go to</button>
     </div>
   );
 }
---
+++ src/demo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const demo = () => {
+  return (
+    <>
+      <h1>Hi brother!</h1>
+    </>
+  );
+};
+
+export default demo;
